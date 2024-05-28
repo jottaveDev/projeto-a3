419c281a21342b27c9899e2f4e533ad58934e035
jottave <joao_victorflamengo10@outlook.com>
2024-05-28 17:43:47 -0300

    format code and add formgroup and navigation

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators as V,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
+
+  form: FormGroup = this.formBuilder.group({
+    email: ['', [V.required, V.email]],
+    password: ['', [V.required, V.minLength(6)]],
+  });
 
   navigateByRegister() {
     this.router.navigate(['/register']);
+  }
+
+  onSubmit() {
+    if (this.form.valid) this.router.navigate(['/home']);
   }
 }
--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators as V,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
+  form: FormGroup = this.formBuilder.group({
+    name: ['', [V.required]],
+    email: ['', [V.required, V.email]],
+    password: ['', [V.required, V.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.form.valid) this.router.navigate(['/login']);
+  }
 }
