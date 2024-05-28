d94c03fca0c5d3ffd109fadfe50ed6245e187e80
jottave <joao_victorflamengo10@outlook.com>
2024-05-28 17:04:46 -0300

    format code and add navigation not register

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  navigateByRegister() {
+    this.router.navigate(['/register']);
+  }
 }
