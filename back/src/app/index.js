9205f3e1e4239a83cc9bdfa30e44a032e55a293c
jottave <joao_victorflamengo10@outlook.com>
2024-05-28 15:58:20 -0300

    add cors middleware and update route post

--- back/src/app/index.js
+++ back/src/app/index.js
@@ -2,15 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-
 const tasks = [
   { id: 1, title: 'Estudar html' },
   { id: 2, title: 'Estudar css' },
   { id: 3, title: 'Estudar js' },
 ];
 
-const getTask = tasks.find((id) => (task) => task.id === parseInt(id));
+function getTask(id) {
+  return tasks.find((task) => task.id === parseInt(id));
+}
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 app.get('/tasks', (request, response) => {
   response.send(tasks);
@@ -23,8 +31,13 @@ app.get('/tasks/:id', (request, response) => {
 });
 
 app.post('/tasks', (request, response) => {
-  tasks.push(request.body);
-  response.send(request.body);
+  const { title } = request.body;
+  const newTask = {
+    id: Math.floor(Math.random() * (100 - 1 + 1)) + 1,
+    title,
+  };
+  tasks.push(newTask);
+  response.send(newTask);
 });
 
 app.put('/tasks/:id', (request, response) => {
