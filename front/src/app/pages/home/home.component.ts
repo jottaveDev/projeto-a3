50225f080f8f35d65dab05f4e2516d9c22d001ae
Carlos Cesar <carloscpnovais@gmail.com>
2024-06-06 20:43:47 -0300

    Fiz a alteração da home da página

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ContactsService } from 'src/app/services/contacts/contacts.service';
+import Task from 'src/app/models/Task.model';
+import { TasksService } from 'src/app/services/tasks/tasks.service';
 
 @Component({
   selector: 'app-home',
@@ -11,40 +12,36 @@ import { ContactsService } from 'src/app/services/contacts/contacts.service';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-  constructor(
-    private contactsService: ContactsService,
-    private router: Router
-  ) {}
+  constructor(private tasksService: TasksService, private router: Router) {}
 
   protected tasks: any;
   protected inputValue: string = '';
+  protected id = localStorage.getItem('token')!;
 
   ngOnInit() {
-    this.contactsService.getTasks().subscribe({
+    this.tasksService.getTasks(this.id).subscribe({
       next: (res) => (this.tasks = res),
-      error: (err) => {
-        console.log(err);
-      },
+      error: (err) => console.log(err),
     });
   }
 
-  addTask(task: any) {
-    this.contactsService.postTask({ title: task }).subscribe({
+  addTask(title: string) {
+    if (title === '') return;
+    this.tasksService.postTask({ title: title, idUser: this.id }).subscribe({
       next: (res) => this.tasks.push(res),
       error: (err) => console.error(err),
     });
   }
 
-  editTask(task: any) {
-    this.router.navigate([`/home/edit/${task.id}`], {
+  editTask(task: Task) {
+    this.router.navigate([`/home/edit/${task.id_task}`], {
       state: { data: task },
     });
   }
 
-  deleteTask(task: any) {
-    this.contactsService.deleteTask(task).subscribe({
-      next: () =>
-        (this.tasks = this.tasks.filter((t: any) => t.id !== task.id)),
+  deleteTask(task: Task) {
+    this.tasksService.deleteTask(task).subscribe({
+      next: () => this.tasks.splice(this.tasks.indexOf(task), 1),
       error: (err) => console.error(err),
     });
   }
